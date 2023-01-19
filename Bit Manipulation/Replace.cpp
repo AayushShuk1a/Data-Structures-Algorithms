7a72fa725d3460e34954c3503a58c6e890dcf808
aayushshuk1a <aayush5emails@gmail.com>
2023-01-19 17:45:59 +0530

    Replace Bits

--- Bit Manipulation/Replace.cpp
+++ Bit Manipulation/Replace.cpp
@@ -1,7 +1,30 @@
 #include<iostream>
 using namespace std;
 
+int clearBitsRange(int m,int i,int j)
+{
+    int a=(-1<<(j+1));
+    int b=(1<<i)-1;
+    int mask=a|b;
+    return m&mask;
+}
+
+int replaceBits(int m,int n,int i)
+{
+    int mask=n<<i;
+    return m|mask;
+}
+
+
 int main()
 {
-    cout<<"hello";
+    int m,n;
+    cin>>m>>n;
+
+    int i,j;
+    cin>>i>>j;
+
+    m=clearBitsRange(m,i,j);
+    cout<<m<<endl;
+    cout<<replaceBits(m,n,i);
 }
