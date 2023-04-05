7b6a281b6d45d704522ffc1eda7533abe886b234
aayushshuk1a <aayush5emails@gmail.com>
2023-04-05 19:47:43 +0530

    Unique Number

--- Bit Manipulation/UniqueNumber.cpp
+++ Bit Manipulation/UniqueNumber.cpp
@@ -19,34 +19,30 @@ int UniqueNumber1(vector<int>vect)
 void UniqueNumber2(vector<int>vect)
 {
 
-    //XOR of all numbers
+    //XOR of all the numbers
     int tempXOR=0;
     for(int i=0;i<vect.size();i++)
     {
         tempXOR=tempXOR^vect[i];
     }
 
+    //find position of ith Bit
 
-
-    //Find position of Set Bit
     int pos=0;
     int temp=tempXOR;
-
     while((temp&1)==0)
     {
         pos++;
-        temp=temp>>1;
+        temp>>1;
     }
 
-
-    //Separate The sets
     int setA=0;
     int setB=0;
     int mask=1<<pos;
 
     for(int i=0;i<vect.size();i++)
     {
-        if((vect[i]&mask)>0)
+        if((vect[i]&mask)==0)
         {
             setA=setA^vect[i];
         }
@@ -54,36 +50,41 @@ void UniqueNumber2(vector<int>vect)
         {
             setB=setB^vect[i];
         }
-
+        
     }
 
     cout<<setA<<endl;
-    cout<<setB;
+    cout<<setB<<endl;
     
 }
+
+
+
+
 
 
 void updateSum(vector<int>&sumArr,int x)
 {
     for(int i=0;i<32;i++)
     {
-        int ithbit=x&(1<<i);
-        if(ithbit)
+        int ithBit=x&(1<<i);
+
+        if(ithBit)
         {
             sumArr[i]++;
         }
     }
-}
 
+}
 
 
 int UniqueNumber3(vector<int>vect)
 {
     vector<int>sumArr(32,0);
 
-    for(auto x:vect)
+    for(int i=0;i<vect.size();i++)
     {
-        updateSum(sumArr,x);
+        updateSum(sumArr,vect[i]);
     }
 
     for(int i=0;i<32;i++)
@@ -91,13 +92,13 @@ int UniqueNumber3(vector<int>vect)
         sumArr[i]=sumArr[i]%3;
     }
 
-    int num=0;
+    int n=0;
     for(int i=0;i<32;i++)
-    {  
-        num+=sumArr[i]*(1<<i);
+    {
+        n+=(sumArr[i]*(1<<i));
     }
 
-    return num;
+    return n;
 
 
 
@@ -106,14 +107,14 @@ int UniqueNumber3(vector<int>vect)
 
 int main()
 {
-    vector<int>vect{5,10,3,10,5,3,4};
-    cout<<"Unique Number 1"<<endl;
-    cout<<UniqueNumber1(vect)<<endl;   //if there is only one unique number and other number are repeating twice (2N+1)
+    // vector<int>vect{5,10,3,10,5,3,4};
+    // cout<<"Unique Number 1"<<endl;
+    // cout<<UniqueNumber1(vect)<<endl;   //if there is only one unique number and other number are repeating twice (2N+1)
 
 
-    cout<<"Unique Number 2"<<endl;
-    vector<int>vect2{5,10,3,10,5,3,4,9};  //if there is 2 unique number and other number are repeating twice (2N+2)
-    UniqueNumber2(vect2);
+    // cout<<"Unique Number 2"<<endl;
+    // vector<int>vect2{5,10,3,10,5,3,4,9};  //if there is 2 unique number and other number are repeating twice (2N+2)
+    // UniqueNumber2(vect2);
 
     cout<<"Unique Number 3"<<endl;
     vector<int>vect3{5,5,5,10,10,10,2,2,2,3,4,4,4};   //One Unique number and other number are repeating thrice (3N+1)
